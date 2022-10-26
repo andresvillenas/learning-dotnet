25885e47d53e287c62a2f41aa693ea73534096f8
Andrés Villenas <andresvillenas@hotmail.com>
2022-10-25 21:30:56 -0500

    My 1st method with C# in 30 sec

---
+++ v2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("1st method with C#");
+
+int SumTwoIntegers(int x, int y)
+{
+    return x + y;
+}
+
+int x = 2, y = 7;
+
+var sum = SumTwoIntegers(x, y);
+
+Console.WriteLine($"{x}+{y}={sum}");
+Console.WriteLine("🎊");
