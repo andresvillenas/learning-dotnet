363e07fd80a687a41d6b67d35de9ec1ebc080d78
Andrés Villenas <andresvillenas@hotmail.com>
2022-11-01 21:10:34 -0500

    v3 - If statement with C# in 30 sec

---
+++ v3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("If statement with C# in 30 sec");
+
+int age = new Random().Next(10,30);
+
+Console.WriteLine($"Your age is: {age}");
+
+if(age >= 21)
+{
+    Console.WriteLine("You can drink ");
+}
+else
+{
+    Console.WriteLine("You cannot drink 🚫 (yet)");
+}
